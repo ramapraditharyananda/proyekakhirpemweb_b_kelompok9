de6c64abca4c7048c614095db6d150455e046600
ramapraditharyananda <ramapro2006@gmail.com>
2026-06-08 00:17:03 +0700

    progress 4

--- app/models/koneksi.php
+++ app/models/koneksi.php
@@ -1,13 +1,16 @@
 <?php
+date_default_timezone_set('Asia/Jakarta');
 $host     = "localhost";
 $dbname   = "umkmify";
 $username = "root";
 $password = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $pdo->exec("SET time_zone = '+07:00'");
 } catch (PDOException $e) {
     die(json_encode(["error" => "Koneksi gagal: " . $e->getMessage()]));
 }
